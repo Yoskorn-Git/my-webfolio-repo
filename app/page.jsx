3a3a6d40615097c9b97480b21d6b9c9ff516d7a1
Yoskorn <57613606+Yoskorn-Git@users.noreply.github.com>
2024-04-08 09:53:34 +0700

    fixed bug

--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,15 @@
 'use client'
 
-import React, { useRef } from 'react'
+import React from 'react'
 import Hero from '@components/Hero'
 import Project from '@components/Project'
 import About from '@components/About'
 import Contact from '@components/Contact'
 import ScrollSpy from "react-ui-scrollspy";
 import NavbarVertical from '@components/NavbarVertical'
-import {useIsVisible} from '@components/SmoothAppear'
 
-const Home = () => {
+const home = () => {
 
-  const ref1 = useRef();
-  const isVisible1 = useIsVisible(ref1);
-  
-  const ref2 = useRef();
-  const isVisible2 = useIsVisible(ref2);
-  
-  const ref3 = useRef();
-  const isVisible3 = useIsVisible(ref2);
 
   return (
     <>
@@ -29,13 +20,13 @@ const Home = () => {
         <section id="home" className='w-full flex justify-center items-center flex-col'>
           <Hero></Hero>
         </section>
-        <section id="about" ref={ref1} className={`transition-opacity ease-in duration-700 ${isVisible1 ? "opacity-100" : "opacity-0"}`}>
+        <section id="about">
           <About />
         </section>
-        <section id="projects"  ref={ref2} className={`transition-opacity ease-in duration-700 ${isVisible2 ? "opacity-100" : "opacity-0"}`}>
+        <section id="projects">
           <Project></Project>
         </section>
-        <section id="contact"  ref={ref3} className={`transition-opacity ease-in duration-700 ${isVisible3 ? "opacity-100" : "opacity-0"}`}>
+        <section id="contact">
           <Contact />
         </section>
       </ScrollSpy>
@@ -45,4 +36,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default home
